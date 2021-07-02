695901d429e0a4df85d313a238d27a3ab0f192ae
jdrae <dr.kiwi.j@gmail.com>
2021-07-03 01:24:58 +0900

    add restcontroller & DI

---
+++ src/main/java/com/practice/spring/controller/MemberController.java
@@ -0,0 +1,41 @@
+package com.practice.spring.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.practice.spring.domain.Member;
+import com.practice.spring.service.MemberService;
+
+@RestController
+public class MemberController{
+	
+	private final MemberService memberService;
+	
+	@ Autowired
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@GetMapping("/members")
+	public List<Member> retrieveMembers(){
+		return memberService.findMembers();
+	}
+	
+	@PostMapping("/members/{name}")
+	ResponseEntity<?> add(@PathVariable String name){
+		Member member = new Member();
+		member.setName(name);
+		Long id = memberService.join(member);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(id).toUri();
+		return ResponseEntity.created(location).build();
+	}
+}
--- src/main/java/com/practice/spring/service/MemberService.java
+++ src/main/java/com/practice/spring/service/MemberService.java
@@ -3,13 +3,17 @@ package com.practice.spring.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.practice.spring.domain.Member;
-import com.practice.spring.repository.MemoryMemberRepository;
 import com.practice.spring.repository.MemberRepository;
 
+@Service
 public class MemberService {
 	private final MemberRepository memberRepository;
 	
+	@Autowired
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
