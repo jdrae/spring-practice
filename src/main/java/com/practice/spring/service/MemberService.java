fa0845a65c29248b60839bcd304c2e21616e00f5
jdrae <dr.kiwi.j@gmail.com>
2021-07-03 00:43:44 +0900

    service & repository

---
+++ src/main/java/com/practice/spring/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.practice.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.practice.spring.domain.Member;
+import com.practice.spring.repository.MemoryMemberRepository;
+import com.practice.spring.repository.MemberRepository;
+
+public class MemberService {
+	private final MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
+	// 회원가입
+	public Long join(Member member) {
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
+	
+	private void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+		.ifPresent(m ->{
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+	}
+
+	// 조회
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	
+	// 상세
+	public Optional<Member> findOne(Long memberId){
+		return memberRepository.findById(memberId);
+	}
+}
